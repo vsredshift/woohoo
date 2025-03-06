586736deeae2d86bac03811faaa16370cb7c4c62
Martin O'Shaughnessy <vsredshift@gmail.com>
2025-03-06 09:46:58 +0100

    add post model and register in admin

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,9 @@
 from django.shortcuts import render
 
-posts = [
-    {
-        "author": "Jack Jackson",
-        "title": "Test Post 1",
-        "content": "Test Content 1",
-        "date_posted": "January 1, 2025",
-    },
-    {
-        "author": "Mary Maryland",
-        "title": "Test Post 2",
-        "content": "Test Content 2",
-        "date_posted": "January 2, 2025",
-    },
-    {
-        "author": "Jack Jackson",
-        "title": "Test Post 3",
-        "content": "Test Content 3",
-        "date_posted": "January 3, 2025",
-    },
-]
-
+from blog.models import Post
 
 def home(request):
-    context = {"posts": posts}
+    context = {"posts": Post.objects.all()}
     return render(request, "blog/index.html", context)
 
 
