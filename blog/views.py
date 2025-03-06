dd12590b79c9f3a69972d6a034823debc60b15ad
Martin O'Shaughnessy <vsredshift@gmail.com>
2025-03-06 15:23:12 +0100

    add CRUD functionality for posts with some auth checks

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,52 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from blog.models import Post
 
-def home(request):
-    context = {"posts": Post.objects.all()}
-    return render(request, "blog/index.html", context)
+
+class PostListView(ListView):
+    model: Post
+    template_name = "blog/index.html"
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        return Post.objects.all().order_by("-date_posted")
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = "/blog/"
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
 
 
 def about(request):
