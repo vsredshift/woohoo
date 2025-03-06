6dda3aa8b468200cc882d755b205ccb1a89a92b3
Martin O'Shaughnessy <vsredshift@gmail.com>
2025-03-06 13:49:46 +0100

    modify user profile, enable editing, upload profile pics

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+from users.models import Profile
+
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
@@ -10,4 +12,25 @@ class UserRegistrationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ["username", "first_name", "last_name", "email", "password1", "password2"]
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "password1",
+            "password2",
+        ]
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ["username", "email"]
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["image"]
--- woohoo/urls.py
+++ woohoo/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
@@ -10,5 +12,10 @@ urlpatterns = [
     path("profile/", user_views.profile, name="profile"),
     path("register/", user_views.register, name="register"),
     path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
-    path("logout/", LogoutView.as_view(template_name="users/logout.html"), name="logout"),
+    path(
+        "logout/", LogoutView.as_view(template_name="users/logout.html"), name="logout"
+    ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
