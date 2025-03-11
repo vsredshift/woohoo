83c511f7791d0fefd7f93dbdd149e7396e860baa
Martin O'Shaughnessy <vsredshift@gmail.com>
2025-03-11 16:59:48 +0100

    fix profile create error

--- users/signals.py
+++ users/signals.py
@@ -7,10 +7,11 @@ from .models import Profile
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
-    if created:
+    if created and not hasattr(instance, "profile"):
         Profile.objects.create(user=instance)
 
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    if hasattr(instance, "profile"):
+        instance.profile.save()
