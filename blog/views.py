13f89d9f5ace9c653ef645c75eee356ea6ebc29d
Martin O'Shaughnessy <vsredshift@gmail.com>
2025-03-11 17:38:09 +0100

    modify about page and footer

--- blog/views.py
+++ blog/views.py
@@ -97,5 +97,6 @@ class AboutView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["hide_sidebar"] = True
+        context["show_socials"] = True
         context["title"] = "About"
         return context
