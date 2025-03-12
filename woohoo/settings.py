020beca2ac003507d63dc7b07cea80ed837076c1
Martin O'Shaughnessy <vsredshift@gmail.com>
2025-03-12 15:51:58 +0100

    update settings

--- woohoo/settings.py
+++ woohoo/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-nb3gu5^ikjy9td($#j3o2tt##_vips(30r#up^627&_t$4r#%+"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
